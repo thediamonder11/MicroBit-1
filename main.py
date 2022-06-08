1a167fe6ee8d78de477964fc65e60244af55af0d
thediamonder11 <76886710+thediamonder11@users.noreply.github.com>
2022-06-08 13:56:37 +0100

    Changed stuff ok.

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 def on_forever():
-    basic.show_arrow(ArrowNames.NORTH)
-    basic.show_arrow(ArrowNames.NORTH_EAST)
-    basic.show_arrow(ArrowNames.EAST)
-    basic.show_arrow(ArrowNames.SOUTH_EAST)
-    basic.show_arrow(ArrowNames.SOUTH)
-    basic.show_arrow(ArrowNames.SOUTH_WEST)
-    basic.show_arrow(ArrowNames.WEST)
-    basic.show_arrow(ArrowNames.NORTH_WEST)
+    basic.show_icon(IconNames.SMALL_HEART)
+    basic.show_icon(IconNames.HEART)
 basic.forever(on_forever)
-
-#This has to be saved in python.
--- main.ts
+++ main.ts
@@ -1,10 +1,4 @@
-basic.forever(function on_forever() {
-    basic.showArrow(ArrowNames.North)
-    basic.showArrow(ArrowNames.NorthEast)
-    basic.showArrow(ArrowNames.East)
-    basic.showArrow(ArrowNames.SouthEast)
-    basic.showArrow(ArrowNames.South)
-    basic.showArrow(ArrowNames.SouthWest)
-    basic.showArrow(ArrowNames.West)
-    basic.showArrow(ArrowNames.NorthWest)
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
 })
