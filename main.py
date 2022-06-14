00074a91c6eb9f2f1dd1994effd0a5e00780ac32
thediamonder11 <76886710+thediamonder11@users.noreply.github.com>
2022-06-14 11:04:15 +0100

    The code now makes a sqare and then clears it.

--- main.py
+++ main.py
@@ -1,17 +1,51 @@
-def on_forever():
-    basic.show_leds("""
-        # . # . #
-                . # . # .
-                # . # . #
-                . # . # .
-                # . # . #
-    """)
-    basic.show_leds("""
-        . # . # .
-                # . # . #
-                . # . # .
-                # . # . #
-                . # . # .
-    """)
-basic.forever(on_forever)
-basic.show_string("L")
+turtle.set_position(0, 0)
+turtle.turn_right()
+turtle.pen(TurtlePenMode.DOWN)
+turtle.forward(4)
+turtle.turn_right()
+turtle.forward(4)
+turtle.turn_right()
+turtle.forward(4)
+turtle.turn_right()
+turtle.forward(4)
+turtle.pen(TurtlePenMode.UP)
+basic.pause(2000)
+basic.show_leds("""
+    . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+""")
+basic.pause(1000)
+basic.show_leds("""
+    . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+""")
+basic.pause(1000)
+basic.show_leds("""
+    . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+""")
+basic.pause(1000)
+basic.show_leds("""
+    . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+""")
+basic.pause(1000)
+basic.show_leds("""
+    # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+""")
--- main.ts
+++ main.ts
@@ -1,16 +1,55 @@
-basic.forever(function () {
-    basic.showLeds(`
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        `)
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        `)
-})
+turtle.setPosition(0, 0)
+turtle.setBrightness(255)
+turtle.turnRight()
+turtle.pen(TurtlePenMode.Down)
+turtle.forward(4)
+turtle.turnRight()
+turtle.forward(4)
+turtle.turnRight()
+turtle.forward(4)
+turtle.turnRight()
+turtle.forward(4)
+turtle.pen(TurtlePenMode.Up)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.showLeds(`
+    # # # # .
+    # . . # .
+    # . . # .
+    # . . # .
+    # # # # .
+    `)
+basic.showLeds(`
+    # # # . .
+    # . # . .
+    # . # . .
+    # . # . .
+    # # # . .
+    `)
+basic.showLeds(`
+    # # . . .
+    # # . . .
+    # # . . .
+    # # . . .
+    # # . . .
+    `)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
